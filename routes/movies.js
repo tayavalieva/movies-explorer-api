ee438a4a681f9b5f7642a96758b4154ffdfe3b66
Taisiia Valieva <taya.skachkova@gmail.com>
2021-12-24 16:00:31 +0000

    feat: initial data validation added via celebrate

--- routes/movies.js
+++ routes/movies.js
@@ -1,15 +1,44 @@
 const moviesRouter = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 
 const { getSavedMovies, addSavedMovie, deleteSavedMovie } = require('../controllers/movies');
+
+const { method } = require('../middlewares/validator');
 
 // gets all movies saved by the current user
 moviesRouter.get('/movies', getSavedMovies);
 
 // creates new saved movie
-moviesRouter.post('/movies', addSavedMovie);
+moviesRouter.post(
+  '/movies',
+  celebrate({
+    body: Joi.object().keys({
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required().custom(method),
+      trailer: Joi.string().required().custom(method),
+      thumbnail: Joi.string().required().custom(method),
+      owner: Joi.string().required().hex(),
+      movieId: Joi.number().required(),
+    }),
+  }),
+  addSavedMovie,
+);
 
 // deletes saved movie
-moviesRouter.delete('/movies/:movieId', deleteSavedMovie);
+moviesRouter.delete(
+  '/movies/:movieId',
+  celebrate({
+    params: Joi.object().keys({
+      movieId: Joi.number().required(),
+    }),
+  }),
+  deleteSavedMovie,
+);
 
 module.exports = moviesRouter;
--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,7 @@ usersRouter.patch(
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().required().min(2).max(30),
-      email: Joi.string().required(),
+      email: Joi.string().required().email(),
     }),
   }),
   updateProfile,
